e7ba804d1a0661ffb80b9d9171c91e6a702180ba
Cristian Orellana <orellana.cristian@hotmail.es>
2025-04-12 18:02:04 -0500

    Añadiendo las columnas para diferenciar los premios que se obtienen por cuota.

--- src/app/core/models/monthlyamortization.model.ts
+++ src/app/core/models/monthlyamortization.model.ts
@@ -4,8 +4,10 @@ export interface MonthlyamortizationModel
     mes: string;
     capital: string;
     interes: string;
+    descuento: string;
     capital_moroso: string;
     interes_moroso: string;
+    descuento_moroso: string;
     total: string;
   }
 
